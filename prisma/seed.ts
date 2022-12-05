0d6ca2ed7aa1d6f94aebc75fb396a03a5b39be7b
retconned <retcnned@gmail.com>
2022-12-05 16:29:33 +0100

    feat: generate guns based on raw file and seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,49 @@
-// import { faker } from "@faker-js/faker";
+import { writeFile } from "fs/promises";
+import rawData from "../data/GONZU_RAW.json";
+import guns from "../data/guns.json";
 import { prisma } from "../src/server/db/client";
 
+// first run processData to generate guns.json then un-comment run to seed the gun file into db
+
+async function processData() {
+  const result: string[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  rawData.map(async (element: any) => {
+    // remove weapon tier & pick rate
+    delete element.tier;
+    delete element.pickRate;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    element.attachments.forEach((element: any) => {
+      // remove unlock data
+      delete element.unlock;
+      // rename attachment type into attachment slot
+      element["slot"] = element["type"];
+      delete element.type;
+    });
+    const proccess = element;
+    // to write into a file
+    result.push(proccess);
+  });
+  await writeFile("./data/guns2.json", JSON.stringify(result));
+}
+
+processData();
+
 async function run() {
-  console.log("");
-  //   const userData = Array(5)
-  //     .fill(null)
-  //     .map(() => {
-  //       return {
-  //         name: faker.internet.userName().toLowerCase(),
-  //         email: faker.internet.email().toLocaleLowerCase(),
-  //         image: faker.image.avatar(),
-  //       };
-  //     });
-  //   const createUsers = userData.map((user) =>
-  //     prisma.user.create({ data: user })
-  //   );
-  // const users = await prisma.$transaction(createUsers);
+  for (const gun of guns) {
+    await prisma.weapon.create({
+      data: {
+        brand: gun.brand,
+        image: gun.image,
+        name: gun.name,
+        type: gun.type,
+        attachments: gun.attachments,
+        bullets: gun.bullets,
+      },
+    });
+  }
   await prisma.$disconnect();
 }
 
