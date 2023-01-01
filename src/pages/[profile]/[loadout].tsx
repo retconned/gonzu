5e6074579f6e4544209f15f2187d99f06ac8f4d8
retconned <retcnned@gmail.com>
2023-01-01 23:31:53 +0100

    feat: adds back button

--- src/pages/[profile]/[loadout].tsx
+++ src/pages/[profile]/[loadout].tsx
@@ -2,7 +2,9 @@ import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import CategoryTitle from "../../components/CategoryTitle";
+import Footer from "../../components/Footer";
 import LoadoutModal from "../../components/LoadoutModal";
+import NavBar from "../../components/NavBar";
 import SmallerLoadout from "../../components/SmallerLoadout";
 import type { LoadoutAttachments } from "../../types/types";
 import { trpc } from "../../utils/trpc";
@@ -12,6 +14,8 @@ const Loadout: NextPage = () => {
   const loadoutId = router.query.loadout;
 
   const profile = router.query.profile as string;
+
+  // console.log(profile);
 
   const [profileLoadouts, setProfileLoadouts] = useState<Array<string>>([""]);
 
@@ -42,7 +46,18 @@ const Loadout: NextPage = () => {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center bg-black text-white">
+    <div className="flex h-full flex-col items-center justify-between bg-neutral-900">
+      <NavBar />
+      <div className="w-8/12 px-4 py-4 text-neutral-200">
+        <button
+          onClick={() => {
+            router.back();
+          }}
+          className="w-fit rounded-md px-3.5 py-2.5  text-left text-sm text-white duration-200 hover:bg-neutral-700 hover:text-lime-400"
+        >
+          {"<  Go Back"}
+        </button>
+      </div>
       <LoadoutModal
         imageSrc={getLoadout.data?.Weapon.image as string}
         loadoutName={getLoadout.data?.loadoutName as string}
@@ -52,24 +67,27 @@ const Loadout: NextPage = () => {
         }
       />
       <CategoryTitle emoji="🔥" title={`Other builds by ${profile}`} />
-      <div className="grid grid-cols-3 gap-2">
-        {profileLoadoutsData ? (
-          profileLoadoutsData.slice(0, 6).map((loadout) => {
-            return (
-              <SmallerLoadout
-                key={loadout.id}
-                loadoutLink={`/${profile}/${loadout.id}`}
-                imageSrc={loadout.Weapon.image as string}
-                loadoutName={loadout.loadoutName}
-                weaponType={loadout.Weapon.type}
-                weaponBody={loadout.weaponBody}
-              />
-            );
-          })
-        ) : (
-          <p>loading</p>
-        )}
+      <div className="flex w-8/12 items-center justify-center">
+        <div className="grid grid-cols-4 gap-6">
+          {profileLoadoutsData ? (
+            profileLoadoutsData.slice(0, 6).map((loadout) => {
+              return (
+                <SmallerLoadout
+                  key={loadout.id}
+                  loadoutLink={`/${profile}/${loadout.id}`}
+                  imageSrc={loadout.Weapon.image as string}
+                  loadoutName={loadout.loadoutName}
+                  weaponType={loadout.Weapon.type}
+                  weaponBody={loadout.weaponBody}
+                />
+              );
+            })
+          ) : (
+            <p>loading</p>
+          )}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
