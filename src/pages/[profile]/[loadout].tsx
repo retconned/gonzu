96e134b4c6693e1ae838f346d56e6a760454698c
retconned <retcnned@gmail.com>
2023-01-01 13:34:33 +0100

    feat: adds recommended section

--- src/pages/[profile]/[loadout].tsx
+++ src/pages/[profile]/[loadout].tsx
@@ -1,6 +1,9 @@
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import CategoryTitle from "../../components/CategoryTitle";
 import LoadoutModal from "../../components/LoadoutModal";
+import SmallerLoadout from "../../components/SmallerLoadout";
 import type { LoadoutAttachments } from "../../types/types";
 import { trpc } from "../../utils/trpc";
 
@@ -8,7 +11,25 @@ const Loadout: NextPage = () => {
   const router = useRouter();
   const loadoutId = router.query.loadout;
 
+  const profile = router.query.profile as string;
+
+  const [profileLoadouts, setProfileLoadouts] = useState<Array<string>>([""]);
+
+  const getProfile = trpc.profile.getUnqiueProfile.useQuery(profile);
   const getLoadout = trpc.loadout.getLoadoutById.useQuery(loadoutId as string);
+
+  useEffect(() => {
+    if (getProfile.isSuccess) {
+      setProfileLoadouts(getProfile.data?.loadouts as string[]);
+    } else {
+      return;
+    }
+  }, [getProfile.isSuccess, getProfile.data]);
+
+  const getProfileLoadouts =
+    trpc.loadout.getProfileLoadout.useQuery(profileLoadouts);
+
+  const profileLoadoutsData = getProfileLoadouts.data;
 
   (getLoadout.data?.attachments as Array<any>)?.forEach((loadedAttach) => {
     getLoadout.data?.Weapon.Attachments.forEach((avalAttachment) => {
@@ -20,11 +41,8 @@ const Loadout: NextPage = () => {
     });
   });
 
-  console.log(getLoadout.data?.attachments);
   return (
-    <div className="bg-black text-white">
-      <p>hello</p>
-      <p>loadout id :{loadoutId}</p>
+    <div className="flex flex-col items-center justify-center bg-black text-white">
       <LoadoutModal
         imageSrc="/m4.png"
         loadoutName={getLoadout.data?.loadoutName as string}
@@ -33,6 +51,25 @@ const Loadout: NextPage = () => {
           getLoadout.data?.attachments as unknown as Array<LoadoutAttachments>
         }
       />
+      <CategoryTitle emoji="🔥" title={`Other builds by ${profile}`} />
+      <div className="flex flex-row gap-2">
+        {profileLoadoutsData ? (
+          profileLoadoutsData.map((loadout) => {
+            return (
+              <SmallerLoadout
+                key={loadout.id}
+                loadoutLink={`/${profile}/${loadout.id}`}
+                imageSrc="/m4.png"
+                loadoutName={loadout.loadoutName}
+                weaponType={loadout.Weapon.type}
+                weaponBody={loadout.weaponBody}
+              />
+            );
+          })
+        ) : (
+          <p>loading</p>
+        )}
+      </div>
     </div>
   );
 };
