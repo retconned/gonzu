6bf29cd802ed14db4a8150cb72db2ea56e533205
retconned <retcnned@gmail.com>
2023-01-01 16:18:37 +0100

    style: loadout and profile builder styling

--- src/components/ProfileBuild.tsx
+++ src/components/ProfileBuild.tsx
@@ -19,7 +19,7 @@ const LoadoutBuilder = () => {
   const { mutate: profileCreate } = trpc.profile.createProfile.useMutation();
   return (
     <>
-      <main className="flex min-h-screen flex-col items-start justify-start bg-gradient-to-b from-[#02246d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-start justify-start ">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Profile
@@ -27,33 +27,42 @@ const LoadoutBuilder = () => {
           <div className="flex flex-col items-center gap-2"></div>
 
           <div>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-3"
+            >
               <input
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 type="text"
                 placeholder="username"
                 {...register("username", {})}
               />
               <input
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 type="text"
                 placeholder="twitch"
                 {...register("twitch", {})}
               />
               <input
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 type="text"
                 placeholder="twitter"
                 {...register("twitter", {})}
               />
               <input
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 type="text"
                 placeholder="youtube"
                 {...register("youtube", {})}
               />
               <input
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 type="text"
                 placeholder="tiktok"
                 {...register("tiktok", {})}
               />
               <select
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
                 {...register("is_streamer", {
                   setValueAs: (v) => Boolean(v),
                 })}
@@ -65,9 +74,13 @@ const LoadoutBuilder = () => {
                 type="text"
                 placeholder="language"
                 {...register("language", {})}
+                className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
               />
 
-              <input type="submit" className="rounded-md bg-red-500 p-2" />
+              <input
+                type="submit"
+                className="rounded-md bg-red-500 p-2 font-bold text-white duration-150 hover:bg-red-700"
+              />
             </form>
           </div>
         </div>
--- src/components/TuneModal.tsx
+++ src/components/TuneModal.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import type { Dispatch, SetStateAction } from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 interface TuneModalProps {
   setVisibility: Dispatch<SetStateAction<boolean>>;
@@ -19,26 +18,34 @@ const TuneModal: NextPage<TuneModalProps> = ({
   const [verticalTune, setVerticalTune] = useState<number>(0);
 
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 flex h-full w-full  items-center justify-center bg-black bg-opacity-20">
-      <div className="flex h-[420px] w-[500px] flex-col items-center justify-center gap-y-4 bg-purple-700 bg-opacity-60 backdrop-blur">
+    <div className="fixed top-0 bottom-0 left-0 right-0 flex h-full w-full  items-center justify-center bg-neutral-900 bg-opacity-60 backdrop-blur-sm">
+      <div className="flex h-[420px] w-[500px] flex-col items-center justify-center gap-y-4 rounded-md bg-neutral-900  text-white ">
+        <p>add tunning amount</p>
         <input
           type="text"
           placeholder="Horizontal Tune"
           onChange={(e) => setHorizontalTune(+e.target.value)}
+          className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center "
         />
         <input
           type="text"
           placeholder="Vertical Tune"
           onChange={(e) => setVerticalTune(+e.target.value)}
+          className="rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
         />
-        <button type="button" onClick={() => setVisibility(false)}>
-          Cancel
-        </button>
         <button
           type="button"
           onClick={() => handleAttachmentAddition(verticalTune, horizontalTune)}
+          className="rounded-md bg-blue-500 p-2 font-bold  hover:bg-blue-700"
         >
           Add Attachment
+        </button>
+        <button
+          type="button"
+          onClick={() => setVisibility(false)}
+          className="rounded-md bg-red-500 p-2 font-bold duration-150 hover:bg-red-700"
+        >
+          Cancel
         </button>
       </div>
     </div>
