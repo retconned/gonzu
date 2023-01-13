735edb7623310bac6091dcb920cbda2b28b270fe
retconned <retcnned@gmail.com>
2023-01-13 05:26:15 +0100

    fix(profilebar): fixes width bug

--- src/components/ProfileBar.tsx
+++ src/components/ProfileBar.tsx
@@ -1,21 +1,10 @@
 import Image from "next/image";
-import {
-  FaInstagram,
-  FaTiktok,
-  FaTwitch,
-  FaTwitter,
-  FaYoutube,
-} from "react-icons/fa";
-
-interface profileBarProps {
-  username: string;
-  imageSrc: string;
-  instagram?: string | null;
-  twitter?: string | null;
-  twitch?: string | null;
-  tiktok?: string | null;
-  youtube?: string | null;
-}
+import type { profileBarProps } from "../types/types";
+import Instagram from "./Socials/Instagram";
+import Tiktok from "./Socials/Tiktok";
+import Twitch from "./Socials/Twitch";
+import Twitter from "./Socials/Twitter";
+import Youtube from "./Socials/Youtube";
 
 const ProfileBar = ({
   username,
@@ -27,7 +16,7 @@ const ProfileBar = ({
   youtube,
 }: profileBarProps) => {
   return (
-    <div className="flex w-8/12 flex-wrap items-center justify-between rounded-md border border-neutral-700 bg-neutral-800 px-10 py-4 text-neutral-200">
+    <div className="flex w-full flex-wrap items-center justify-between rounded-md border border-neutral-700 bg-neutral-800 px-10 py-4 text-neutral-200">
       <div className="flex flex-row items-center space-x-4 ">
         <div className=" flex items-center gap-4 text-neutral-200 ">
           <Image
@@ -44,36 +33,11 @@ const ProfileBar = ({
         </div>
       </div>
       <div className="flex flex-row items-center justify-center space-x-4">
-        <a href={twitch != null ? `${twitch}` : ""}>
-          <FaTwitch
-            size={22}
-            className="fill-lime-400 duration-100 hover:fill-lime-600"
-          />
-        </a>
-        <a href={twitter != null ? `${twitter}` : ""}>
-          <FaTwitter
-            size={22}
-            className="fill-lime-400 duration-100 hover:fill-lime-600"
-          />
-        </a>
-        <a href={youtube != null ? `${youtube}` : ""}>
-          <FaYoutube
-            size={22}
-            className="fill-lime-400 duration-100 hover:fill-lime-600"
-          />
-        </a>
-        <a href={tiktok != null ? `${tiktok}` : ""}>
-          <FaTiktok
-            size={22}
-            className="fill-lime-400 duration-100 hover:fill-lime-600"
-          />
-        </a>
-        <a href={instagram != null ? `${instagram}` : ""}>
-          <FaInstagram
-            size={22}
-            className="fill-lime-400 duration-100 hover:fill-lime-600"
-          />
-        </a>
+        <Twitter twitter={twitter} />
+        <Twitch twitch={twitch} />
+        <Youtube youtube={youtube} />
+        <Tiktok tiktok={tiktok} />
+        <Instagram instagram={instagram} />
       </div>
     </div>
   );
