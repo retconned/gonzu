73a04de2c58378c319a1f1a429230423d2b150ea
retconned <retcnned@gmail.com>
2023-02-15 20:06:30 +0100

    style: polishes on onboard page

--- src/pages/auth/onboarding.tsx
+++ src/pages/auth/onboarding.tsx
@@ -15,7 +15,7 @@ const OnBoarding = () => {
   // console.log(sessionData?.user?.name);
   // console.log(sessionData);
   return (
-    <div className="flex h-screen flex-col justify-between bg-blue-300">
+    <div className="flex h-screen flex-col justify-between bg-neutral-900">
       <NavBar />
       <div className="flex">
         <ProfileOnBoarder sessionData={sessionData} />
@@ -47,86 +47,76 @@ const ProfileOnBoarder = ({ sessionData }: any) => {
     trpc.profile.createProfileOnboarding.useMutation();
 
   return (
-    <>
-      <main className="flex flex-col items-start justify-start px-20">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-2xl font-extrabold text-white">
+    <div className="items-cent flex w-full flex-col justify-center">
+      <main className="flex flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-center ">
+          <h1 className=" text-2xl font-extrabold text-white">
             {"You're almost there"}
           </h1>
           <div>
             <form
               onSubmit={handleSubmit(onSubmit)}
-              className=" flex flex-col gap-3 text-white"
+              className="flex w-[500px] flex-col gap-3 text-white"
             >
-              {/* <input
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                placeholder="username"
-                disabled
-                // value={sessionData?.user?.name}
-                {...register("username", {
-                  value: sessionData?.user?.name,
-                })}
-              />
-              <input
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                disabled
-                placeholder="@twitch"
-                {...register("twitch", {
-                  value: sessionData?.user?.name,
-                })}
-                // value={sessionData?.user?.name}
-              /> */}
-              <input
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                placeholder="@twitter"
-                {...register("twitter", {})}
-              />
-              <input
-                className="rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                placeholder="youtube (url)"
-                {...register("youtube", {})}
-              />
-              <input
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                placeholder="tiktok"
-                {...register("tiktok", {})}
-              />
-              <label>Instagram</label>
-              <input
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-                type="text"
-                placeholder="instagram"
-                {...register("instagram", {})}
-              />
-              <label>Are you a streamer?</label>
-              <select
-                className="form-select rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
-                {...register("is_streamer", {
-                  setValueAs: (v) => Boolean(v),
-                })}
-              >
-                <option value={"true"}>true</option>
-                <option value={""}>false</option>
-              </select>
-              <select
-                className="form-select rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
-                {...register("input", {})}
-              >
-                <option value={"mouse"}>🖱 mouse</option>
-                <option value={"controller"}>🎮 controller</option>
-              </select>
-              <input
-                type="text"
-                placeholder="language"
-                {...register("language", {})}
-                className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
-              />
-
+              <div className="flex flex-row justify-between ">
+                <div className="flex w-full flex-col gap-4 p-4">
+                  <label>Twitter</label>
+                  <input
+                    className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
+                    type="text"
+                    placeholder="@twitter"
+                    {...register("twitter", {})}
+                  />
+                  <label>Youtube</label>
+                  <input
+                    className="rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
+                    type="text"
+                    placeholder="youtube (url)"
+                    {...register("youtube", {})}
+                  />
+                  <label>Tiktok</label>
+                  <input
+                    className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
+                    type="text"
+                    placeholder="tiktok"
+                    {...register("tiktok", {})}
+                  />
+                  <label>Instagram</label>
+                  <input
+                    className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
+                    type="text"
+                    placeholder="instagram"
+                    {...register("instagram", {})}
+                  />
+                </div>
+                <div className="flex w-full flex-col gap-4 p-4">
+                  <label>Are you a streamer?</label>
+                  <select
+                    className="form-select rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
+                    {...register("is_streamer", {
+                      setValueAs: (v) => Boolean(v),
+                    })}
+                  >
+                    <option value={"true"}>Yes</option>
+                    <option value={""}>No</option>
+                  </select>
+                  <label>What do you use</label>
+                  <select
+                    className="form-select rounded-md bg-neutral-800 p-1 text-center placeholder:text-center"
+                    {...register("input", {})}
+                  >
+                    <option value={"mouse"}>🖱 mouse</option>
+                    <option value={"controller"}>🎮 controller</option>
+                  </select>
+                  <label>Language/Region</label>
+                  <input
+                    type="text"
+                    placeholder="language"
+                    {...register("language", {})}
+                    className="form-input rounded-md bg-neutral-800 p-1 text-center placeholder-neutral-400 placeholder:text-center"
+                  />
+                </div>
+              </div>
               <input
                 type="submit"
                 className="rounded-md bg-red-500 p-2 font-bold text-white duration-150 hover:bg-red-700"
@@ -135,6 +125,6 @@ const ProfileOnBoarder = ({ sessionData }: any) => {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 };
